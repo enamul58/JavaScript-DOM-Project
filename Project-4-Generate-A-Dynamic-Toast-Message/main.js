7e310b1b9cf6d81dc663286ca18c62657da55717
“enamul58” <enamullimon@gmail.com>
2023-11-27 16:36:11 +0600

    add toast message box when copy button click

--- Project-4-Generate-A-Dynamic-Toast-Message/main.js
+++ Project-4-Generate-A-Dynamic-Toast-Message/main.js
@@ -2,6 +2,7 @@ const inputFieldValue = document.getElementById("assignHexValue");
 const myButton = document.querySelector("#myButton");
 const bodyTag = document.getElementsByTagName("body");
 const copyBtn = document.querySelector("#cpy_btn");
+let div = null;
 
 myButton.addEventListener("click", function () {
   const hexValue = getHexValue();
@@ -11,6 +12,7 @@ myButton.addEventListener("click", function () {
 
 copyBtn.addEventListener("click", function () {
   navigator.clipboard.writeText(inputFieldValue.value);
+  generateToastMessage(`${inputFieldValue.value} copied`);
 });
 
 function getHexValue() {
@@ -18,4 +20,25 @@ function getHexValue() {
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
   return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+function generateToastMessage(message) {
+  if (div !== null) {
+    div.remove();
+    div = null;
+  }
+  div = document.createElement("div");
+  div.className = "toast_message toast_message_in";
+  div.innerText = message;
+  document.body.appendChild(div);
+
+  div.addEventListener("click", function () {
+    div.classList.remove("toast_message_in");
+    div.classList.add("toast_message_out");
+
+    div.addEventListener("animationend", function () {
+      div.remove();
+      div = null;
+    });
+  });
 }
